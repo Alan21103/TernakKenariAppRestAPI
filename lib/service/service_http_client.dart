5b7c658605d663b9c52b3f8c24ec9c559d2adb65
Alan21103 <syahlansaputra03@gmail.com>
2025-06-06 21:13:28 +0700

    Revisi base url dan menambahkan fungsi token

--- lib/service/service_http_client.dart
+++ lib/service/service_http_client.dart
@@ -4,25 +4,30 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 class ServiceHttpClient {
-  final String baseUrl = 'http://10.0.0.2:8000/api/';
+  final String baseUrl = 'http://10.0.2.2:8000/api/';
   final secureStorage = FlutterSecureStorage();
 
   // POST
-  Future<http.Response> post(String endpoint, Map<String, dynamic> body) async {
+  Future<http.Response> postWithToken(
+    String endpoint,
+    Map<String, dynamic> body,
+  ) async {
+    final token = await secureStorage.read(key: 'token');
     final url = Uri.parse("$baseUrl$endpoint");
 
     try {
       final response = await http.post(
         url,
         headers: {
-          'Accept' : 'application/json',
-          'Content-Type' : 'application/json',
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          if (token != null) 'Authorization': 'Bearer $token',
         },
         body: jsonEncode(body),
       );
       return response;
     } catch (e) {
-      throw Exception("POST request failed: $e");
+      throw Exception("POST request with token failed: $e");
     }
   }
 
@@ -42,6 +47,25 @@ class ServiceHttpClient {
       return response;
     } catch (e) {
       throw Exception("GET request failed: $e");
+    }
+  }
+
+  // POST tanpa token
+  Future<http.Response> post(String endpoint, Map<String, dynamic> body) async {
+    final url = Uri.parse("$baseUrl$endpoint");
+
+    try {
+      final response = await http.post(
+        url,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(body),
+      );
+      return response;
+    } catch (e) {
+      throw Exception("POST request failed: $e");
     }
   }
 }
