8501649b941ba134d89de226a44d25bd08e643ca
Alan21103 <syahlansaputra03@gmail.com>
2025-06-06 21:10:28 +0700

    implementasi penyimpanan token autentikasi dan role user ke secure storage saat login

--- lib/data/repository/auth_repository.dart
+++ lib/data/repository/auth_repository.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
 
-import 'package:canary_app/data/model/request/login_request_model.dart';
-import 'package:canary_app/data/model/request/register_request_model.dart';
-import 'package:canary_app/data/model/response/login_response_model.dart';
+import 'package:canary_app/data/model/request/auth/login_request_model.dart';
+import 'package:canary_app/data/model/request/auth/register_request_model.dart';
+import 'package:canary_app/data/model/response/auth_response_model.dart';
 import 'package:canary_app/service/service_http_client.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:dartz/dartz.dart';
@@ -13,51 +13,86 @@ class AuthRepository {
 
   AuthRepository(this._serviceHttpClient);
 
-  Future<Either<String, LoginResponseModel>> login(
-    LoginRequestModel requestModel,
-  ) async {
-    try {
-      final response = await _serviceHttpClient.post(
-        "login",
-        requestModel.toMap(),
-      );
-      final jsonResponse = json.decode(response.body);
-      if (response.statusCode == 200) {
-        final loginResponse = LoginResponseModel.fromMap(jsonResponse);
-        await secureStorage.write(
-          key: "authToken",
-          value: loginResponse.user!.token,
-        );
-        await secureStorage.write(
-          key: "userRole",
-          value: loginResponse.user!.role,
-        );
-        return Right(loginResponse);
-      } else {
-        return Left(jsonResponse['message'] ?? "Login failed");
-      }
-    } catch (e) {
-      return Left("An error occured while logging in.");
-    }
-  }
+  // Future<Either<String, LoginResponseModel>> login(
+  //   LoginRequestModel requestModel,
+  // ) async {
+  //   try {
+  //     final response = await _serviceHttpClient.postWithToken(
+  //       "login",
+  //       requestModel.toMap(),
+  //     );
+  //     final jsonResponse = json.decode(response.body);
+  //     if (response.statusCode == 200) {
+  //       final loginResponse = LoginResponseModel.fromMap(jsonResponse);
+  //       await secureStorage.write(
+  //         key: "authToken",
+  //         value: loginResponse.user!.token,
+  //       );
+  //       await secureStorage.write(
+  //         key: "userRole",
+  //         value: loginResponse.user!.role,
+  //       );
+  //       return Right(loginResponse);
+  //     } else {
+  //       return Left(jsonResponse['message'] ?? "Login failed");
+  //     }
+  //   } catch (e) {
+  //     return Left("An error occured while logging in.");
+  //   }
+  // }
 
   Future<Either<String, String>> register(
     RegisterRequestModel requestModel,
   ) async {
     try {
+      // Gunakan post tanpa token untuk register
       final response = await _serviceHttpClient.post(
         "register",
         requestModel.toMap(),
       );
       final jsonResponse = json.decode(response.body);
-      final registerResponse = jsonResponse['message'];
+
       if (response.statusCode == 200) {
-        return Right(registerResponse);
+        // Ambil pesan sukses dari response
+        final registerMessage =
+            jsonResponse['message'] ?? "Registrasi berhasil";
+        return Right(registerMessage);
       } else {
-        return Left(jsonResponse['message'] ?? "Registration failed");
+        // Error dari server
+        return Left(jsonResponse['message'] ?? "Registrasi gagal");
       }
     } catch (e) {
-      return Left("An error occured while registering.");
+      return Left("Terjadi kesalahan saat registrasi.");
+    }
+  }
+
+  Future<Either<String, AuthResponseModel>> login(
+    LoginRequestModel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.post(
+        'login',
+        requestModel.toMap(),
+      );
+      final jsonResponse = json.decode(response.body);
+      if (response.statusCode == 200) {
+        final authResponse = AuthResponseModel.fromMap(jsonResponse);
+        if (authResponse.user?.token != null) {
+          await secureStorage.write(
+            key: "authToken",
+            value: authResponse.user!.token!,
+          );
+          await secureStorage.write(
+            key: "userRole",
+            value: authResponse.user!.role ?? '',
+          );
+        }
+        return Right(authResponse);
+      } else {
+        return Left(jsonResponse['message'] ?? 'Login failed');
+      }
+    } catch (e) {
+      return Left("An error occured while logging in: $e");
     }
   }
 }
